e924a0216e4d872271ef9d79e5797071a109ccb1
Harlequelrah <degbovimax19@gmail.com>
2025-06-24 15:51:38 +0000

    Add data tables and update line chart styling
    Introduces new data table components for orders and users, enhancing dashboard functionality and data visibility. Updates line chart appearance for improved readability and visual consistency. Adds dependencies for table and UI components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Calendar from "../components/Cards/Calendar"
+import { DataTableDemo } from "../components/Cards/DataTable"
 import General from "../components/Cards/General"
 import Lines from "../components/Cards/Lines"
-import { Card } from "../components/ui/card"
+import { TableDemo } from "../components/Cards/Table"
+import { Card, CardDescription, CardHeader, CardTitle } from "../components/ui/card"
 
 export default function Home() {
   return <div className="grid gap-[32px]">
@@ -12,10 +14,26 @@ export default function Home() {
         <Calendar />
       </div>
     </div>
-    <div className="grid grid-cols-3 gap-[32px]">
+    <div className="grid grid-cols-3 gap-[32px] h-[300px] overflow-hidden">
       <Lines />
-      <Card className="h-[300px]">Hello</Card>
-      <Card className="h-[300px]">Hello</Card>
+      <Card className="overflow-y-scroll">
+        <CardHeader>
+          <CardTitle>Orders</CardTitle>
+          <CardDescription>These are the orders of this month.</CardDescription>
+        </CardHeader>
+        <div className="px-4">
+          <TableDemo />
+        </div>
+      </Card>
+      <Card className="overflow-y-scroll">
+        <CardHeader>
+          <CardTitle>User</CardTitle>
+          <CardDescription>These are the users of this month.</CardDescription>
+        </CardHeader>
+        <div className="px-4">
+          <DataTableDemo />
+        </div>
+      </Card>
     </div>
   </div>
 }
